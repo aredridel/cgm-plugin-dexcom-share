3f4d8b70c5d7a7d8e09423730e881c6524f1e3cd
Aria Stewart <aredridel@dinhe.net>
2019-11-05 21:41:43 -0500

    Finish reworking plugin to use fetch()

--- index.js
+++ index.js
@@ -8,145 +8,159 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *
  * @description: Allows user to store their Dexcom data in their own
  * Nightscout server by facilitating the transfer of latest records
  * from Dexcom's server into theirs.
  */
-var request = require('request');
-var qs = require('querystring');
-var crypto = require('crypto');
-
+const fetch = require("node-fetch");
+const qs = require("querystring");
+const crypto = require("crypto");
 
 // Defaults
-var server = "share1.dexcom.com";
-var bridge = readENV('BRIDGE_SERVER')
-    if (bridge && bridge.indexOf(".") > 1) {
-    server = bridge;
-   } 
-    else if (bridge && bridge === 'EU') {
-        server = "shareous1.dexcom.com";
-    } 
+const bridge = getenv("DEXCOM_SERVER");
+const server = bridge == "US" ? "share1.dexcom.com"
+  : bridge == "EU" ? "shareous1.dexcom.com"
+  : bridge && bridge.indexOf(".") > 1 ? bridge
+  : "share1.dexcom.com";
 
-var Defaults = {
-  "applicationId":"d89443d2-327c-4a6f-89e5-496bbb0317db"
-, "agent": "Dexcom Share/3.0.2.11 CFNetwork/711.2.23 Darwin/14.0.0"
-, login: 'https://' + server + '/ShareWebServices/Services/General/LoginPublisherAccountByName'
-, accept: 'application/json'
-, 'content-type': 'application/json'
-, LatestGlucose: 'https://' + server + '/ShareWebServices/Services/Publisher/ReadPublisherLatestGlucoseValues'
-// ?sessionID=e59c836f-5aeb-4b95-afa2-39cf2769fede&minutes=1440&maxCount=1"
-, MIN_PASSPHRASE_LENGTH: 12
+const Defaults = {
+  applicationId: "d89443d2-327c-4a6f-89e5-496bbb0317db",
+  agent: "Dexcom Share/3.0.2.11 CFNetwork/711.2.23 Darwin/14.0.0",
+  login: `https://${server}/ShareWebServices/Services/General/LoginPublisherAccountByName`,
+  accept: "application/json",
+  "content-type": "application/json",
+  LatestGlucose: `https://${server}/ShareWebServices/Services/Publisher/ReadPublisherLatestGlucoseValues`,
+  // ?sessionID=e59c836f-5aeb-4b95-afa2-39cf2769fede&minutes=1440&maxCount=1"
+  MIN_PASSPHRASE_LENGTH: 12
 };
 
-var DIRECTIONS = {
-  NONE: 0
-, DoubleUp: 1
-, SingleUp: 2
-, FortyFiveUp: 3
-, Flat: 4
-, FortyFiveDown: 5
-, SingleDown: 6
-, DoubleDown: 7
-, 'NOT COMPUTABLE': 8
-, 'RATE OUT OF RANGE': 9
+const DIRECTIONS = {
+  NONE: 0,
+  DoubleUp: 1,
+  SingleUp: 2,
+  FortyFiveUp: 3,
+  Flat: 4,
+  FortyFiveDown: 5,
+  SingleDown: 6,
+  DoubleDown: 7,
+  "NOT COMPUTABLE": 8,
+  "RATE OUT OF RANGE": 9
 };
-var Trends = (function ( ) {
-  var keys = Object.keys(DIRECTIONS);
-  var trends = keys.sort(function (a, b) {
+
+const Trends = (() => {
+  const keys = Object.keys(DIRECTIONS);
+  const trends = keys.sort(function(a, b) {
     return DIRECTIONS[a] - DIRECTIONS[b];
   });
   return trends;
-})( );
-function directionToTrend (direction) {
-  var trend = 8;
+})();
+
+function directionToTrend(direction) {
   if (direction in DIRECTIONS) {
-    trend = DIRECTIONS[direction];
+    return DIRECTIONS[direction];
+  } else {
+    return DIRECTIONS["NOT COMPUTABLE"];
   }
-  return trend;
 }
-function trendToDirection (trend) {
+
+function trendToDirection(trend) {
   return Trends[trend] || Trends[0];
 }
 
 // assemble the POST body for the login endpoint
-function login_payload (opts) {
+function login_payload(opts) {
   var body = {
-    "password": opts.password
-  , "applicationId" : opts.applicationId || Defaults.applicationId
-  , "accountName": opts.accountName
+    password: opts.password,
+    applicationId: opts.applicationId || Defaults.applicationId,
+    accountName: opts.accountName
   };
   return body;
 }
 
 // Login to Dexcom's server.
-function authorize (opts, then) {
-  var url = Defaults.login;
-  var body = login_payload(opts);
-  var headers = { 'User-Agent': Defaults.agent
-                , 'Content-Type': Defaults['content-type']
-                , 'Accept': Defaults.accept };
-  var req ={ uri: url, body: body, json: true, headers: headers, method: 'POST'
-           , rejectUnauthorized: false };
-  // Asynchronously calls the `then` function when the request's I/O
-  // is done.
-  return request(req, then);
+function authorize(opts) {
+  const url = Defaults.login;
+  const body = login_payload(opts);
+  const headers = {
+    "User-Agent": Defaults.agent,
+    "Content-Type": Defaults["content-type"],
+    Accept: Defaults.accept
+  };
+  const req = {
+    body: JSON.stringify(body),
+    headers,
+    method: "POST"
+  };
+
+  return _fetch(url, req); 
+}
+
+async function _fetch(url, opts) {
+  const res = await fetch(url, opts);
+  if (res.ok) {
+    return await res.json();
+  } else {
+    const body = await res.text();
+    throw new Error(`Status code ${res.status}, ${body}`);
+  }
 }
 
 // Assemble query string for fetching data.
-function fetch_query (opts) {
+function fetch_query(opts) {
   // ?sessionID=e59c836f-5aeb-4b95-afa2-39cf2769fede&minutes=1440&maxCount=1"
-  var q = {
-    sessionID: opts.sessionID
-  , minutes: opts.minutes || 1440
-  , maxCount: opts.maxCount || 1
+  const q = {
+    sessionID: opts.sessionID,
+    minutes: opts.minutes || 1440,
+    maxCount: opts.maxCount || 1
   };
-  var url = Defaults.LatestGlucose + '?' + qs.stringify(q);
-  return url;
+  return `${Defaults.LatestGlucose}?${qs.stringify(q)}`;
 }
 
 // Asynchronously fetch data from Dexcom's server.
 // Will fetch `minutes` and `maxCount` records.
-function fetch (opts, then) {
+function fetchData(opts) {
   var url = fetch_query(opts);
   var body = "";
-  var headers = { 'User-Agent': Defaults.agent
-                , 'Content-Type': Defaults['content-type']
-                , 'Content-Length': 0
-                , 'Accept': Defaults.accept };
+  var headers = {
+    "User-Agent": Defaults.agent,
+    "Content-Type": Defaults["content-type"],
+    "Content-Length": 0,
+    Accept: Defaults.accept
+  };
 
-  var req ={ uri: url, body: body, json: true, headers: headers, method: 'POST'
-           , rejectUnauthorized: false };
-  return request(req, then);
+  var req = {
+    body: body,
+    json: true,
+    headers: headers,
+    method: "POST",
+    rejectUnauthorized: false
+  };
+  return _fetch(url, req);
 }
 
 // Authenticate and fetch data from Dexcom.
-function do_everything (opts, then) {
+async function do_everything(opts, then) {
   var login_opts = opts.login;
   var fetch_opts = opts.fetch;
-  authorize(login_opts, function (err, res, body) {
-
-    fetch_opts.sessionID = body;
-    fetch(fetch_opts, function (err, res, glucose) {
-      then(err, glucose);
-
-    });
-  });
-
+  const sessionID = await authorize(login_opts);
+  fetch_opts.sessionID = body;
+  const glucose = await fetchData(fetch_opts);
+  return glucose;
 }
 
 // Map Dexcom's property values to Nightscout's.
-function dex_to_entry (d) {
-/*
+function dex_to_entry(d) {
+  /*
 [ { DT: '/Date(1426292016000-0700)/',
     ST: '/Date(1426295616000)/',
     Trend: 4,
@@ -155,65 +169,60 @@ function dex_to_entry (d) {
 */
   var regex = /\((.*)\)/;
   var wall = parseInt(d.WT.match(regex)[1]);
-  var date = new Date(wall);
   var entry = {
-    sgv: d.Value
-  , date: wall
-  , dateString: date.toISOString( )
-  , trend: d.Trend
-  , direction: trendToDirection(d.Trend)
-  , device: 'share2'
-  , type: 'sgv'
+    sgv: d.Value,
+    ts: wall,
+    trend: d.Trend,
+    direction: trendToDirection(d.Trend),
+    device: "share2",
+    type: "sgv"
   };
   return entry;
 }
 
-function engine (opts) {
-
+function engine(opts) {
   var runs = 0;
   var failures = 0;
-  function my ( ) {
-    console.log('RUNNING', runs, 'failures', failures);
+  async function my() {
+    console.log("RUNNING", runs, "failures", failures);
     if (my.sessionID) {
       var fetch_opts = Object.create(opts.fetch);
       if (runs === 0) {
-        console.log('First run, fetching', opts.firstFetchCount);
+        console.log("First run, fetching", opts.firstFetchCount);
         fetch_opts.maxCount = opts.firstFetchCount;
       }
       fetch_opts.sessionID = my.sessionID;
-      fetch(fetch_opts, function (err, res, glucose) {
-        if (res && res.statusCode < 400) {
-          to_nightscout(glucose);
-        } else {
-          my.sessionID = null;
-          refresh_token( );
-        }
-      });
+      try {
+        const glucose = await fetchData(fetch_opts)
+        to_nightscout(glucose);
+      } catch (e) {
+        console.warn(e);
+        my.sessionID = null;
+        return refresh_token();
+      }
     } else {
       failures++;
-      refresh_token( );
+      return refresh_token();
     }
   }
 
-  function refresh_token ( ) {
-    console.log('Fetching new token');
-    authorize(opts.login, function (err, res, body) {
-      if (!err && body && res && res.statusCode == 200) {
-        my.sessionID = body;
-        failures = 0;
-        my( );
-      } else {
-        failures++;
-        var responseStatus = res ? res.statusCode : "response not found";
-        console.log("Error refreshing token", err, responseStatus, body);
-        if (failures >= opts.maxFailures) {
-          throw "Too many login failures, check BRIDGE_USER_NAME and BRIDGE_PASSWORD";
-        }
+  async function refresh_token() {
+    console.log("Fetching new token");
+    try {
+      const sessionID = await authorize(opts.login);
+      my.sessionID = sessionID;
+      failures = 0;
+      return my();
+    } catch (err) {
+      failures++;
+      console.warn("Error refreshing token", err);
+      if (failures >= opts.maxFailures) {
+        throw "Too many login failures, check DEXCOM_USER_NAME and DEXCOM_PASSWORD";
       }
-    });
+    }
   }
 
-  function to_nightscout (glucose) {
+  function to_nightscout(glucose) {
     if (glucose) {
       runs++;
       // Translate to Nightscout data.
@@ -222,87 +231,47 @@ function engine (opts) {
     }
   }
 
-  my( );
+  my();
   return my;
 }
 
-// Provide public, testable API
-engine.fetch = fetch;
-engine.authorize = authorize;
-engine.authorize_fetch = do_everything;
-module.exports = engine;
-
-function readENV(varName, defaultValue) {
-    //for some reason Azure uses this prefix, maybe there is a good reason
-    var value = process.env['CUSTOMCONNSTR_' + varName]
-        || process.env['CUSTOMCONNSTR_' + varName.toLowerCase()]
-        || process.env[varName]
-        || process.env[varName.toLowerCase()];
-
-    return value || defaultValue;
+function getenv(varName, defaultValue) {
+  const value = process.env[varName]
+  return value || defaultValue;
 }
 
 // If run from commandline, run the whole program.
-if (!module.parent) {
-  if (readENV('BRIDGE_USER_NAME', '@').match(/\@/)) {
-    var msg = [ "environment variable"
-              , "BRIDGE_USER_NAME should be"
-              , "Dexcom Share user name, not an email address"];
-    var err = new Error(msg.join(' '));
-    throw err;
-    process.exit(1);
-  }
-  var args = process.argv.slice(2);
-  var config = {
-    accountName: readENV('BRIDGE_USER_NAME')
-  , password: readENV('BRIDGE_PASSWORD')
-  };
-  var interval = readENV('SHARE_INTERVAL', 60000 * 2.5);
-  interval = Math.max(60000, interval);
-  var fetch_config = { maxCount: readENV('maxCount', 1)
-    , minutes: readENV('minutes', 1440)
-  };
-  var meta = {
-    login: config
-  , fetch: fetch_config
-  , maxFailures: readENV('maxFailures', 3)
-  , firstFetchCount: readENV('firstFetchCount', 3)
-  };
-  switch (args[0]) {
-    case 'login':
-      authorize(config, console.log.bind(console, 'login'));
-      break;
-    case 'fetch':
-      config = { sessionID: args[1] };
-      fetch(config, console.log.bind(console, 'fetched'));
-      break;
-    case 'testdaemon':
-      setInterval(engine(meta), 2500);
-      break;
-    case 'run':
-      // Authorize and fetch from Dexcom.
-      do_everything(meta, function (err, glucose) {
-        console.log('From Dexcom', err, glucose);
-        if (glucose) {
-          // Translate to Nightscout data.
-          var entries = glucose.map(dex_to_entry);
-          log_entries(entries);
-        }
-      });
-      break;
-    default:
-      setInterval(engine(meta), interval);
-      break;
-      break;
-  }
+if (getenv("DEXCOM_USER_NAME", "@").match(/\@/)) {
+  throw new Error("environment variable DEXCOM_USER_NAME should be Dexcom Share user name, not an email address");
 }
+
+const args = process.argv.slice(2);
+
+const config = {
+  accountName: getenv("DEXCOM_USER_NAME"),
+  password: getenv("DEXCOM_PASSWORD")
+};
+
+const interval = Math.max(60000, getenv("SHARE_INTERVAL", 60000 * 2.5));
+const fetch_config = {
+  maxCount: getenv("maxCount", 1),
+  minutes: getenv("minutes", 1440)
+};
+const meta = {
+  login: config,
+  fetch: fetch_config,
+  maxFailures: getenv("maxFailures", 3),
+  firstFetchCount: getenv("firstFetchCount", 3)
+};
+
+setInterval(engine(meta), interval);
 
 function log_entries(entries) {
   for (const entry of entries) {
     if (global.postMessage) {
-      global.postMessage(entry) 
+      global.postMessage(entry);
     } else {
-      console.log(JSON.stringify(entry))
+      console.log(JSON.stringify(entry));
     }
   }
 }
